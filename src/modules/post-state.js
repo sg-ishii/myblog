b9bfd5f71df19cc331ace5637f0bcc3b64180320
koki <kioskhii@kokinoMacBook-Air.local>
2020-01-25 12:41:04 +0900

    call api

--- src/modules/post-state.js
+++ src/modules/post-state.js
@@ -1,11 +1,18 @@
 
-const ALREADY_READ = 'ALREADY_READ';
+import axios from "axios"
+import Cookies from 'universal-cookie';
+import uuidv4 from 'uuid/v4'
+
+const cookies = new Cookies();
+const ALREADY_READ = 'ALREADY_READ'
+const ADD_FUNC_URL = 'https://api.sg-ishii.page'
 
 const initialState = { reads: [] }
 
 export default function postReducer (state=initialState, action) {
     if (action.type === ALREADY_READ) {
         if (!state.reads.includes(action.slugId)) {
+            addFunc(action.slugId)
             const reads = state.reads.concat(action.slugId,)
             return {
                 ...state,
@@ -17,7 +24,21 @@ export default function postReducer (state=initialState, action) {
     return state
 }
 
-export const already_read = slugId => {
+const addFunc = (slugId) => {
+
+    const uuid = cookies.get('uuid') ? cookies.get('uuid') : uuidv4()
+
+    axios.post(ADD_FUNC_URL, {uuid: uuid, slag: slugId})
+    .then(res => {
+        console.log('success')
+    })
+    .catch(e => {
+        console.log(e)
+        cookies.set('uuid', uuid)
+    })
+}
+
+export const already_read = (slugId) => {
     return {
         type: ALREADY_READ,
         slugId: slugId
