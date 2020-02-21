f4d92c04b490f96c147d1ec45ecf1b63ec7bf7fa
koki <kioskhii@kokinoMacBook-Air.local>
2020-02-22 00:06:27 +0900

    yaml fix

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "start make env file"
-echo GATSBY_API_KEY=$_API_KEY >> .env.production
-echo GATSBY_AUTH_DOMAIN=$_AUTH_DOMAIN >> .env.production
-echo GATSBY_PROJECT_ID=$PROJECT_ID >> .env.production
+echo GATSBY_API_KEY=${_API_KEY} >> .env.production
+echo GATSBY_AUTH_DOMAIN=${_AUTH_DOMAIN} >> .env.production
+echo GATSBY_PROJECT_ID=${PROJECT_ID} >> .env.production
 echo "end make env file"
 cat .env.production
